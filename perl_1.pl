6068d6c7bf45d6477ca75390fc1f86ec382c49ca
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2024-01-06 23:08:18 +0530

    Perl Begining

---
+++ perl_1.pl
@@ -0,0 +1,14 @@
+# This is the first program of perl 
+# From now ownwards we will start perl and in this we learnt we should strict and warning to have warning and know what is wrong
+# !/usr/bin/env perl
+use strict;
+use warnings;
+use 5.0.2;
+
+
+my $test = "Mohan";
+
+print  "\nLakshay Said Hello\n";
+print $test. " Said Hi\n";
+1;
+# $employee_name = "lakshay";
